03e8c2b94a44838f0d7e4d6b0ce24792389a83de
saul00x <saulhugo71@gmail.com>
2025-09-17 23:47:10 +0100

    Updated logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,15 +1,1 @@
-@@ .. @@
- export function Logo() {
-   return (
-     <div className="relative w-24 h-24">
-       <Image
--        src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/logo%20sdfm-gDlxg0zxe6wVV9o5cISteykVa4LQhz.png"
--        alt="SDFM 2520"
-+        src="/logo.png"
-+        alt="Bergomi Store"
-         fill
-         className="object-contain"
-         priority
-@@ .. @@
-   )
- }
+{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
